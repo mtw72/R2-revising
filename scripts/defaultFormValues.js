3f242c2ab081d7258590a4230a0bca6d3531de6e
mtw72 <mtwong72@gmail.com>
2024-03-22 18:31:45 +0000

    revised defaultFormValues - 2

--- scripts/defaultFormValues.js
+++ scripts/defaultFormValues.js
@@ -1,38 +1,45 @@
 // default date and time values in reservation form
 // get date of today
 const dateOfToday = new Date();
+const tdyDay = dateOfToday.getDay();
 let tdyDate = dateOfToday.getDate();
 let tdyMth = dateOfToday.getMonth() + 1;
-let tdyYear = dateOfToday.getFullYear();
-let tdyHour = dateOfToday.getHours();
+const tdyYear = dateOfToday.getFullYear();
+const tdyHour = dateOfToday.getHours();
+
+//make the date and/or month in 2-digit format
 if (tdyDate < 10) {
   tdyDate = "0" + tdyDate;
 }
 if (tdyMth < 10) {
   tdyMth = "0" + tdyMth;
 }
+
 const today = tdyYear + "-" + tdyMth + "-" + tdyDate;
 
 // get date of tomorrow
 const dateOfTmr = new Date(new Date().setDate(dateOfToday.getDate() + 1));
 let tmrDate = dateOfTmr.getDate();
 let tmrMth = dateOfTmr.getMonth() + 1;
-let tmrYear = dateOfTmr.getFullYear();
+const tmrYear = dateOfTmr.getFullYear();
+
+//make the date and/or month in 2-digit format
 if (tmrDate < 10) {
   tmrDate = "0" + tmrDate;
 }
 if (tmrMth < 10) {
   tmrMth = "0" + tmrMth;
 }
+
 const tomorrow = tmrYear + "-" + tmrMth + "-" + tmrDate;
 
 
-// date picker - set default date and prevent choosing invalid dates
+// date picker - set default date (.value) and prevent choosing invalid dates (.min)
 const dateInput = document.getElementById('date');
 
-switch (dateOfToday.getDay()) {
-  case 0:
-    if (tdyHour > 15) { //Sunday
+switch (tdyDay) {
+  case 0: //Sunday
+    if (tdyHour > 15) {
       dateInput.value = tomorrow;
       dateInput.min = tomorrow;
 
@@ -42,8 +49,8 @@ switch (dateOfToday.getDay()) {
     }
     break;
   case 5:
-  case 6:
-    if (tdyHour > 19) { //Friday & Saturday
+  case 6: //Friday & Saturday
+    if (tdyHour > 19) {
       dateInput.value = tomorrow;
       dateInput.min = tomorrow;
     } else {
@@ -51,8 +58,8 @@ switch (dateOfToday.getDay()) {
       dateInput.min = today;
     }
     break;
-  default:
-    if (tdyHour > 18) { //Monday to Thursday
+  default: //Monday to Thursday
+    if (tdyHour > 18) {
       dateInput.value = tomorrow;
       dateInput.min = tomorrow;
     } else {
@@ -62,7 +69,7 @@ switch (dateOfToday.getDay()) {
 }
 
 
-// time picker - set default time and prevent choosing invalid hours (before opening time / during operating time)
+// time picker - set default time (.defaultValue) and prevent choosing invalid hours (.min: before opening time / during operating time)
 const timeInput = document.getElementById('time');
 const defaultTime = (tdyHour + 2) + ":" + ("00"); //default time
 const validHour = (tdyHour + 1) + ":" + ("00");   //valid booking time
@@ -77,18 +84,16 @@ if (dateInput.value === today) {
     timeInput.min = "12:00";
   }
 } else {
-  // for another day, set the default time to 12nn
+  // for another day (tomorrow), set the default time to 12nn
   timeInput.value = "12:00";
   timeInput.min = "12:00";
 }
 
-// after closing time
+// time picker - prevent choosing invalid hours (.max: before closing time)
 // booking using default date
-const dayoftoday = dateOfToday.getDay();
-
-if (dayoftoday === 0) { // Sunday
+if (tdyDay === 0) { // Sunday
   timeInput.max = '16:00';
-} else if (dayoftoday === 5 || dayoftoday === 6) { // Friday & Saturday
+} else if (tdyDay === 5 || tdyDay === 6) { // Friday & Saturday
   timeInput.max = '20:00';
 } else { // Monday to Thursday
   timeInput.max = '19:00';
@@ -98,7 +103,7 @@ if (dayoftoday === 0) { // Sunday
 dateInput.addEventListener('input', (event) => {
   const selectedDate = new Date(event.target.value);
   const dayOfWeek = selectedDate.getDay();
-  // timeInput.min = '12:00';
+  timeInput.min = '12:00';
   if (dayOfWeek === 0) { // Sunday
     timeInput.max = '16:00';
   } else if (dayOfWeek === 5 || dayOfWeek === 6) { // Friday & Saturday
