715979595ddd883121458324b4f0e4ed008b2f8d
mtw72 <mtwong72@gmail.com>
2024-03-23 16:07:00 +0000

    add aria-selected to large menu tabs

--- scripts/largeMenu.js
+++ scripts/largeMenu.js
@@ -1,20 +1,22 @@
 function openMenu(event, menuName) {
-  let i, menus, menutabs;
-  menus = document.getElementsByClassName("large-food-menu");
-  for (i = 0; i < menus.length; i++) {
-    menus[i].style.display = "none";
-    menus[i].setAttribute('aria-hidden', true);
-  }
-  document.getElementById(menuName).style.display = "grid";
-  document.getElementById(menuName).setAttribute('aria-hidden', false);
+  let i, menutabs, menus;
 
   menutabs = document.getElementsByClassName("large-menu__tab");
-  for (i = 0; i < menus.length; i++) {
+  for (i = 0; i < menutabs.length; i++) {
     menutabs[i].classList.remove("large-menu__tab--active");
     menutabs[i].setAttribute('aria-selected', 'false');
   }
-  event.currentTarget.parentElement.classList.add("large-menu__tab--active");
-  event.currentTarget.parentElement.setAttribute('aria-selected', 'true');
+  event.currentTarget.classList.add("large-menu__tab--active");
+  event.currentTarget.setAttribute('aria-selected', 'true');
+
+
+  menus = document.getElementsByClassName("large-food-menu");
+  for (i = 0; i < menus.length; i++) {
+    menus[i].style.display = "none";
+    menus[i].setAttribute('aria-hidden', 'true');
+  }
+  document.getElementById(menuName).style.display = "grid";
+  document.getElementById(menuName).setAttribute('aria-hidden', 'false');
 }
 
 document.getElementById("myButton").click();
