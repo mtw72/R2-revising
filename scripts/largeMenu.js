192b78f079c1c55c4a860ed26023df77eb2ced4e
mtw72 <mtwong72@gmail.com>
2024-03-23 16:37:31 +0000

    reivsed aria-attribute in large menu

--- scripts/largeMenu.js
+++ scripts/largeMenu.js
@@ -5,18 +5,20 @@ function openMenu(event, menuName) {
   for (i = 0; i < menutabs.length; i++) {
     menutabs[i].classList.remove("large-menu__tab--active");
     menutabs[i].setAttribute('aria-selected', 'false');
+    menutabs[i].setAttribute('tabindex', '-1');
   }
   event.currentTarget.classList.add("large-menu__tab--active");
   event.currentTarget.setAttribute('aria-selected', 'true');
+  event.currentTarget.removeAttribute('tabindex', '-1');
 
 
   menus = document.getElementsByClassName("large-food-menu");
   for (i = 0; i < menus.length; i++) {
     menus[i].style.display = "none";
-    menus[i].setAttribute('aria-hidden', 'true');
+    // menus[i].setAttribute('aria-hidden', 'true');
   }
   document.getElementById(menuName).style.display = "grid";
-  document.getElementById(menuName).setAttribute('aria-hidden', 'false');
+  // document.getElementById(menuName).setAttribute('aria-hidden', 'false');
 }
 
-document.getElementById("myButton").click();
+document.getElementById("pasta-button").click();
