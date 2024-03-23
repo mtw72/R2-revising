c69438bb1c09d6a5fcbf377a2b0f52e0885630dc
mtw72 <mtwong72@gmail.com>
2024-03-23 12:54:13 +0000

    large menu aria attribute

--- scripts/largeMenu.js
+++ scripts/largeMenu.js
@@ -3,14 +3,18 @@ function openMenu(event, menuName) {
   menus = document.getElementsByClassName("large-food-menu");
   for (i = 0; i < menus.length; i++) {
     menus[i].style.display = "none";
+    menus[i].setAttribute('aria-hidden', true);
   }
   document.getElementById(menuName).style.display = "grid";
+  document.getElementById(menuName).setAttribute('aria-hidden', false);
 
   menutabs = document.getElementsByClassName("large-menu__tab");
   for (i = 0; i < menus.length; i++) {
     menutabs[i].classList.remove("large-menu__tab--active");
+    menutabs[i].setAttribute('aria-expanded', 'false');
   }
   event.currentTarget.parentElement.classList.add("large-menu__tab--active");
+  event.currentTarget.parentElement.setAttribute('aria-expanded', 'true');
 }
 
 document.getElementById("myLink").click();
