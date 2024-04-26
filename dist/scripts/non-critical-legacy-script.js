65f89c903850699acac7633a2ee7871b362375ab
mtw72 <mtwong72@gmail.com>
2024-04-26 18:57:50 +0100

    navbar done! focus on the hero-image

--- dist/scripts/non-critical-legacy-script.js
+++ dist/scripts/non-critical-legacy-script.js
@@ -56,24 +56,31 @@ function closeNavbarByTab(event) {
 }
 
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
-// let prevScrollpos = window.scrollY;
-// window.onscroll = function () {
-//   let currentScrollPos = window.scrollY;
-//   let screenWidth = window.innerWidth;
-
-//   if (prevScrollpos > currentScrollPos) {
-//     navBar.style.top = "0";
-//     if (screenWidth <= 350 || screenWidth <= 600 && screenWidth > 450) {
-//       home.style.paddingTop = "70px";
-//     }
-//   } else {
-//     navBar.style.top = "-500px";
-//     home.style.paddingTop = "0px";
-//     closeNavbar();
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
+var prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY;
+  var distanceFromTop = home.offsetTop - currentScrollPos;
+  var screenWidth = window.innerWidth;
+  if (prevScrollpos > currentScrollPos) {
+    navBar.style.top = "0";
+    // Check if the user is within a certain distance from the hero section
+    if (distanceFromTop < 200 && distanceFromTop > -200) {
+      if (screenWidth <= 350 || screenWidth <= 600 && screenWidth > 450) {
+        // Adjust padding only when the user is near the hero section at certain screen width
+        home.style.paddingTop = "70px";
+      }
+    }
+  } else {
+    navBar.style.top = "-500px";
+    closeNavbar();
+    if (distanceFromTop < 200 && distanceFromTop > -200) {
+      if (screenWidth <= 350 || screenWidth <= 600 && screenWidth > 450) {
+        home.style.paddingTop = "0px";
+      }
+    }
+  }
+  prevScrollpos = currentScrollPos;
+};
 window.onresize = function () {
   // Update the screenWidth variable with the current window width
   var screenWidth = window.innerWidth;
