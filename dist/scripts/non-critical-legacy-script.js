b03435ee0af1832f5e9067753cd2e48c83ca116a
mtw72 <mtwong72@gmail.com>
2024-05-01 00:46:51 +0100

    navbar a11y

--- dist/scripts/non-critical-legacy-script.js
+++ dist/scripts/non-critical-legacy-script.js
@@ -17,10 +17,12 @@ navbarToggler.addEventListener('click', function (event) {
   navList.classList.toggle('is-opened');
   if (navList.style.maxHeight) {
     navList.style.maxHeight = null;
+    navList.setAttribute('inert', 'true');
     navbarToggler.setAttribute('aria-expanded', 'false');
     negativeTabIndex();
   } else {
     navList.style.maxHeight = navList.scrollHeight + "px";
+    navList.setAttribute('inert', 'false');
     navbarToggler.setAttribute('aria-expanded', 'true');
     zeroTabIndex();
   }
@@ -40,6 +42,7 @@ function closeNavbar() {
   if (navList.classList.contains('is-opened')) {
     navList.style.maxHeight = null;
     navList.classList.remove('is-opened');
+    navList.setAttribute('inert', 'true');
     navbarToggler.setAttribute('aria-expanded', 'false');
     negativeTabIndex();
   }
@@ -86,20 +89,24 @@ window.onresize = function () {
 // add or remove aria-attributes values of menu toggler
 function addDefaultAriaAttributes() {
   navbarToggler.setAttribute('aria-expanded', 'false');
+  navList.setAttribute('inert', 'true');
 }
 function removeDefaultAriaAttributes() {
   navbarToggler.removeAttribute('aria-expanded', 'false');
+  navList.removeAttribute('inert', 'true');
 }
 
 // tabindex of navlinks
 function zeroTabIndex() {
   for (var i = 0; i < navLinks.length; i++) {
     navLinks[i].setAttribute('tabindex', '0');
+    navLinks[i].setAttribute('aria-hidden', 'false');
   }
 }
 function negativeTabIndex() {
   for (var i = 0; i < navLinks.length; i++) {
     navLinks[i].setAttribute('tabindex', '-1');
+    navLinks[i].setAttribute('aria-hidden', 'true');
   }
 }
 window.addEventListener('load', checkScreenSize);
--- dist/scripts/non-critical-modern-script.js
+++ dist/scripts/non-critical-modern-script.js
@@ -18,11 +18,12 @@ navbarToggler.addEventListener('click', (event) => {
   navList.classList.toggle('is-opened');
   if (navList.style.maxHeight) {
     navList.style.maxHeight = null;
+    navList.setAttribute('inert', 'true');
     navbarToggler.setAttribute('aria-expanded', 'false');
-
     negativeTabIndex();
   } else {
     navList.style.maxHeight = navList.scrollHeight + "px";
+    navList.setAttribute('inert', 'false');
     navbarToggler.setAttribute('aria-expanded', 'true');
     zeroTabIndex();
   }
@@ -43,6 +44,7 @@ function closeNavbar() {
   if (navList.classList.contains('is-opened')) {
     navList.style.maxHeight = null;
     navList.classList.remove('is-opened');
+    navList.setAttribute('inert', 'true');
     navbarToggler.setAttribute('aria-expanded', 'false');
     negativeTabIndex();
   }
@@ -94,22 +96,26 @@ window.onresize = function () {
 // add or remove aria-attributes values of menu toggler
 function addDefaultAriaAttributes() {
   navbarToggler.setAttribute('aria-expanded', 'false');
+  navList.setAttribute('inert', 'true');
 }
 
 function removeDefaultAriaAttributes() {
   navbarToggler.removeAttribute('aria-expanded', 'false');
+  navList.removeAttribute('inert', 'true');
 }
 
 // tabindex of navlinks
 function zeroTabIndex() {
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].setAttribute('tabindex', '0');
+    navLinks[i].setAttribute('aria-hidden', 'false');
   }
 }
 
 function negativeTabIndex() {
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].setAttribute('tabindex', '-1');
+    navLinks[i].setAttribute('aria-hidden', 'true');
   }
 }
 
