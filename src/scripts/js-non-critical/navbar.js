6a9845ad0c5d5215191eb8bdd9d10b038f3c30fd
mtw72 <mtwong72@gmail.com>
2024-05-10 00:58:16 +0100

    revising navbar.js_v2

--- src/scripts/js-non-critical/navbar.js
+++ src/scripts/js-non-critical/navbar.js
@@ -1,44 +1,68 @@
 'use strict';
 
+// Get the elements inside the navbar and the home section
 const navBar = document.getElementById("navbar");
 const navbarToggler = document.querySelector('.navbar__toggler');
 const navList = document.querySelector('.navbar__collapse');
 const navLinks = document.querySelectorAll('.navbar__nav-link');
+const findusLink = document.getElementById('findus-link');
 const home = document.getElementById("home");
+
 
 // Show or hide the collapsible navbar when toggler is clicked
 navbarToggler.addEventListener('click', (event) => {
+  // Toggle the visibility of navList
   navList.classList.toggle('is-opened');
   if (navList.style.maxHeight) {
+    // If navList is open, close it
     navList.style.maxHeight = null;
-    navbarToggler.setAttribute('aria-expanded', 'false');
-    negativeTabIndex(); //set the navlinks to be aria-hidden and tab-index = -1
+    // Set the toggler NOT to be aria-expanded
+    togglerAriaNotExpanded();
+    // Set the navlinks to be aria-hidden and tab-index = -1
+    navlinkAriaHidden();
   } else {
+    // If navList is closed, open it
     navList.style.maxHeight = navList.scrollHeight + "px";
-    navbarToggler.setAttribute('aria-expanded', 'true');
-    zeroTabIndex(); //set the navlinks NOT to be aria-hidden and tab-index = 0
+    // Set the toggler to be aria-expanded
+    togglerAriaExpanded();
+    // Set the navlinks NOT to be aria-hidden and tab-index = 0
+    navlinkAriaNotHidden();
   }
   event.stopPropagation();
 });
 
 
+// Functions to set / remove the aria attribute(s) of toggler (aria-expanded)
+function togglerAriaExpanded() {
+  navbarToggler.setAttribute('aria-expanded', 'true');
+}
 
-// Functions to change the tabindex and aria-hidden attribute of navlinks
-function negativeTabIndex() {
+function togglerAriaNotExpanded() {
+  navbarToggler.setAttribute('aria-expanded', 'false');
+}
+
+function togglerAriaRemoved() {
+  navbarToggler.removeAttribute('aria-expanded', 'true');
+  navbarToggler.removeAttribute('aria-expanded', 'false');
+}
+
+
+// Functions to set / remove the aria attributes of navlinks (tabindex and aria-hidden)
+function navlinkAriaHidden() {
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].setAttribute('tabindex', '-1');
     navLinks[i].setAttribute('aria-hidden', 'true');
   }
 }
 
-function zeroTabIndex() {
+function navlinkAriaNotHidden() {
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].setAttribute('tabindex', '0');
     navLinks[i].setAttribute('aria-hidden', 'false');
   }
 }
 
-function removeTabIndex() {
+function navlinkAriaRemoved() {
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].removeAttribute('tabindex', '0');
     navLinks[i].removeAttribute('tabindex', '-1');
@@ -47,16 +71,16 @@ function removeTabIndex() {
   }
 }
 
+
 // Function to close the collapsible navbar
 function closeNavbar() {
   if (navList.classList.contains('is-opened')) {
     navList.style.maxHeight = null;
     navList.classList.remove('is-opened');
-    navbarToggler.setAttribute('aria-expanded', 'false');
-    negativeTabIndex();
+    togglerAriaNotExpanded();
+    navlinkAriaHidden();
   }
 }
-
 
 
 // Hide the collapsible navbar when the nav link is clicked 
@@ -66,15 +90,14 @@ document.addEventListener('click', () => {
 });
 
 
-
 // For keyboard user, close the navbar if the key "TAB" is pressed
 // let the navbar stay open if the key "SHIFT" + "TAB" are pressed
-const findusLink = document.getElementById('findus-link');
 findusLink.addEventListener('keydown', closeNavbarByTab);
 
 function closeNavbarByTab(event) {
   const keyCode = event.keyCode || event.which;
-  if (event.shiftKey && event.keyCode == 9) { // Check if the key pressed is 'tab'
+  // Check if the key pressed is 'tab'
+  if (event.shiftKey && event.keyCode == 9) {
     // act normally if pressing "shift" + "tab" (going backwards)
   } else if (keyCode === 9) {
     closeNavbar();
@@ -82,52 +105,9 @@ function closeNavbarByTab(event) {
 }
 
 
-
-// ** On Screen resize
-// Close the navbar when the page is loaded or the screen is re-sized
-// On small screen, set the aria-expanded attribute of the toggler, set the navlinks to be aria-hidden and tab-index = -1
-// On large screen, remove the aria-expanded attribute of the toggler, remove aria-hidden and tab-index attributes of navlinks
-window.addEventListener('load', checkScreenSize);
-window.addEventListener('resize', checkScreenSize);
-
-function checkScreenSize() {
-  closeNavbar();
-
-  let screenWidth = window.innerWidth;
-  if (screenWidth <= 576) {
-    addAriaExpandedAttributes();
-    negativeTabIndex();
-  } else {
-    removeAriaExpandedAttributes();
-    removeTabIndex();
-  }
-}
-
-function addAriaExpandedAttributes() {
-  navbarToggler.setAttribute('aria-expanded', 'false');
-}
-
-function removeAriaExpandedAttributes() {
-  navbarToggler.removeAttribute('aria-expanded', 'false');
-}
-
-// Adjust the padding top of the hero image according to the screen size
-window.onresize = function () {
-  // Update the screenWidth variable with the current window width
-  let screenWidth = window.innerWidth;
-
-  // Check the screenWidth and adjust value of paddingTop accordingly
-  if (screenWidth <= 350 || (screenWidth <= 600 && screenWidth > 450)) {
-    home.style.paddingTop = "70px";
-  } else {
-    home.style.paddingTop = "0px";
-  }
-}
-
-
-
-// ** On Scroll
-// On screen wider than 900px, when the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
+// On Scroll
+// On screen wider than 900px, when the user scrolls down, hide the navbar. 
+// When the user scrolls up, show the navbar 
 let prevScrollPos = window.scrollY;
 
 window.onscroll = function () {
@@ -144,3 +124,51 @@ window.onscroll = function () {
 
   prevScrollPos = currentScrollPos;
 }
+
+
+// Function to adjust the padding top of the hero image according to the screen size
+function adjustHeroImagePadding() {
+  // Update the screenWidth variable with the current window width
+  let screenWidth = window.innerWidth;
+
+  // Check the screenWidth and adjust value of paddingTop accordingly
+  if (screenWidth <= 350 || (screenWidth <= 600 && screenWidth > 450)) {
+    home.style.paddingTop = "70px";
+  } else {
+    home.style.paddingTop = "0px";
+  }
+}
+
+
+// Function to check the screen size and assign aria attributes to HTML elements
+// For use when onload and onresize
+function checkScreenSize() {
+  let screenWidth = window.innerWidth;
+  // On small screen, set the toggler to be aria-expanded,
+  // set the navlinks to be aria-hidden and tab-index = -1
+  if (screenWidth <= 576) {
+    togglerAriaNotExpanded();
+    navlinkAriaHidden();
+  }
+  // On large screen, remove the aria-expanded attribute of the toggler,
+  // remove aria-hidden and tab-index attributes of navlinks
+  else {
+    togglerAriaRemoved();
+    navlinkAriaRemoved();
+  }
+}
+
+
+// On Screen resize
+// 1. Close the navbar
+// 2. Check the screen size and assign appropriate aria attributes to HTML elements
+// 3. Check if needed to adjust the padding-top value of hero-image
+window.addEventListener('resize', function () {
+  closeNavbar();
+  checkScreenSize();
+  adjustHeroImagePadding();
+});
+
+
+//Check the screen size onload and assign appropriate aria attributes to HTML elements
+window.addEventListener('load', checkScreenSize);
